5792fdd85ebb308a77e08740a75d6afaaf2f7804
diego <di3goandres@icloud.com>
2020-11-25 12:23:49 -0500

    ajuste mortalidad lado administrador

--- TruchasSurala/src/app/componentes/10-Mortalidad/registromortalidad/registromortalidad.component.ts
+++ TruchasSurala/src/app/componentes/10-Mortalidad/registromortalidad/registromortalidad.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatStepper } from '@angular/material/stepper';
 import { MatTableDataSource } from '@angular/material/table';
+import { Respuestas, RegistroDiario } from 'src/app/models/mortalidad/mortalidad.detalle';
 import { Registro } from 'src/app/models/mortalidad/mortalidad.response';
 import { DetalleMortalidadUsuario } from 'src/app/models/mortalidad/mortalidad.usuarios';
 import { MortalidadService } from '../../../service/mortalidad/mortalidad.service';
@@ -16,11 +17,11 @@ export class RegistromortalidadComponent implements OnInit {
   @ViewChild('stepper', { static: false }) stepper: MatStepper;
 
   displayedColumns: string[] = ['position', 'fecha', 'total_ovas', 'numero_ovas',
-  'ovas_regalo', 'ovas_adicionales',
-  'ovas_reposicion', 'cantidad', 'porcentaje', 'detalle'];
+    'ovas_regalo', 'ovas_adicionales',
+    'ovas_reposicion', 'cantidad', 'porcentaje', 'detalle'];
 
-  displayedColumnsRegistros: string[] = ['position','fecha', 'nombre', 'finca',
-  'total', 'cantidad','porcentaje'];
+  displayedColumnsRegistros: string[] = ['position', 'fecha', 'nombre', 'finca',
+    'total', 'cantidad', 'porcentaje', 'detalle'];
   public dataSource = new MatTableDataSource<Registro>();
   public dataSourceDetalle = new MatTableDataSource<DetalleMortalidadUsuario>();
 
@@ -28,7 +29,11 @@ export class RegistromortalidadComponent implements OnInit {
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   detalleUsuarios: DetalleMortalidadUsuario[];
 
-
+  // Pantalla 3
+  Respuestas : Respuestas;
+  RegistroDiario: RegistroDiario[];
+  fotos: any[];
+  displayedColumnsDias: string[] = ['dia', 'cantidad', 'registrado']
   constructor(
     private service: MortalidadService
   ) { }
@@ -39,7 +44,8 @@ export class RegistromortalidadComponent implements OnInit {
 
   cargainicial() {
     this.service.traerInformacion().subscribe(
-      OK => { console.log(OK)
+      OK => {
+
         this.registros = [];
         this.registros.push(...OK.registros)
         this.dataSource = new MatTableDataSource(this.registros);
@@ -49,19 +55,35 @@ export class RegistromortalidadComponent implements OnInit {
     )
   }
 
-  VerRegistros(registro :  Registro){
+  VerRegistros(registro: Registro) {
     this.service.traerRegistros(registro.id).subscribe(
-          OK => {console.log(OK)
+      OK => {
+        console.log(OK)
 
-            this.detalleUsuarios =[];
-            this.detalleUsuarios.push(...OK.detalleUsuarios)
-            this.dataSourceDetalle = new MatTableDataSource(this.detalleUsuarios);
-            this.dataSourceDetalle.paginator = this.paginator;
-            this.stepper.next()
-          
-          },
-          ERROR => {console.log(ERROR)},
-        )
+        this.detalleUsuarios = [];
+        this.detalleUsuarios.push(...OK.detalleUsuarios)
+        this.dataSourceDetalle = new MatTableDataSource(this.detalleUsuarios);
+        this.dataSourceDetalle.paginator = this.paginator;
+        this.stepper.next()
+
+      },
+      ERROR => { console.log(ERROR) },
+    )
+  }
+
+  usuario: DetalleMortalidadUsuario;
+  VerDetalle(usuario: DetalleMortalidadUsuario) {
+    this.usuario = usuario;
+    this.service.traerRegistrosDiarios(usuario.id_mortalidad).subscribe(
+      OK => {
+        console.log(OK)
+        this.Respuestas = OK.Respuestas;
+        this.RegistroDiario = [];
+        this.RegistroDiario.push(...OK.RegistroDiario)
+        this.stepper.next()
+      },
+      ERROR => { console.log(ERROR) },
+    )
   }
 
 }
--- TruchasSurala/src/app/service/mortalidad/mortalidad.service.ts
+++ TruchasSurala/src/app/service/mortalidad/mortalidad.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { UserService } from '../user/user.service';
 import { MortalidadResponse } from '../../models/mortalidad/mortalidad.response';
 import { MortalidadUsuariosRespose } from '../../models/mortalidad/mortalidad.usuarios';
+import { DetalleMortalidadResponse } from '../../models/mortalidad/mortalidad.detalle';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,9 @@ export class MortalidadService {
   }
   traerRegistros(id){
     return this.service.ejecutarQuery<MortalidadUsuariosRespose>('/api/mortalidad/UsuarioregistrosMortalidad/' + id);
+  }
+
+  traerRegistrosDiarios(id){
+    return this.service.ejecutarQuery<DetalleMortalidadResponse>('/api/mortalidad/getregistrosDiarioMortalidad/' + id);
   }
 }
