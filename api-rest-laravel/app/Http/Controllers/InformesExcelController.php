e58e59cf281f67479f3cfa6f5564aac4ee288a2b
diego <di3goandres@icloud.com>
2021-03-12 20:07:32 -0500

    d

--- api-rest-laravel/app/Http/Controllers/InformesExcelController.php
+++ api-rest-laravel/app/Http/Controllers/InformesExcelController.php
@@ -12,11 +12,11 @@ class InformesExcelController extends Controller
     //
     public function __construct()
     {
-           $this->middleware('api.auth');
+        $this->middleware('api.auth');
     }
 
     public function Mortalidad(Request $request)
     {
-        return Excel::download(new MortalidadTotalExport, 'MortalidadesPendientes'. time() .'.xlsx');
+        return Excel::download(new MortalidadTotalExport, 'MortalidadesPendientes' . time() . '.xlsx');
     }
 }
